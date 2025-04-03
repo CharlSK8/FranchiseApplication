6d4d251adc8de76785a65687cda54f8097cedc69
Charlinson Perez <charlinson2013@gmail.com>
2025-04-03 09:45:24 -0500

    feat: añade interfaz IFranchiseService

---
+++ src/main/java/com/franchises/develop/service/IFranchiseService.java
@@ -0,0 +1,15 @@
+package com.franchises.develop.service;
+
+import com.franchises.develop.dto.request.BranchDTO;
+import com.franchises.develop.dto.request.BranchUpdateNameRequestDTO;
+import com.franchises.develop.dto.request.FranchiseRequestDTO;
+import com.franchises.develop.dto.request.FranchiseUpdateNameRequestDTO;
+import com.franchises.develop.dto.response.ProductWithBranchDTO;
+import com.franchises.develop.dto.response.ResponseDTO;
+import com.franchises.develop.model.Franchise;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public interface IFranchiseService {
+}
