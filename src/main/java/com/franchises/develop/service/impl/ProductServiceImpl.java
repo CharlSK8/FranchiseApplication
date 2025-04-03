0b2fd264adebd323d6bdd92d14ec86ebbf23ffb1
Charlinson Perez <charlinson2013@gmail.com>
2025-04-03 13:20:46 -0500

    feat: Crea la clase ProductServiceImpl que implementa la interfaz IProductService.

---
+++ src/main/java/com/franchises/develop/service/impl/ProductServiceImpl.java
@@ -0,0 +1,20 @@
+package com.franchises.develop.service.impl;
+
+import com.franchises.develop.dto.response.ResponseDTO;
+import com.franchises.develop.exception.handler.ProductAlreadyExistsException;
+import com.franchises.develop.exception.handler.ProductNameAlreadyUpToDateException;
+import com.franchises.develop.exception.handler.ProductNotFoundException;
+import com.franchises.develop.model.Product;
+import com.franchises.develop.repository.ProductRepository;
+import com.franchises.develop.service.IProductService;
+import com.franchises.develop.util.Constants;
+import com.franchises.develop.util.ErrorHandlerUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@AllArgsConstructor
+public class ProductServiceImpl implements IProductService {
+}
