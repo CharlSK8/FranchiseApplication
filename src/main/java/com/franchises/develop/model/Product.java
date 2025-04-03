9a2083696f1143585722c12947dd19e6bdf918ca
Charlinson Perez <charlinson2013@gmail.com>
2025-04-03 11:25:45 -0500

    feat: Crea el documento Product en MongoDB para representar las colecciones de products.

---
+++ src/main/java/com/franchises/develop/model/Product.java
@@ -0,0 +1,21 @@
+package com.franchises.develop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "products")
+public class Product {
+
+    @Id
+    private String id;
+    private String name;
+    private int stock;
+}
