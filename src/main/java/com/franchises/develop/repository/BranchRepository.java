c5d1d5433dc179d64f8102d8e88fd193a958af2c
Charlinson Perez <charlinson2013@gmail.com>
2025-04-03 16:22:28 -0500

    feat: Crea el método findByName en BranchRepository para buscar sucursales por nombre.

--- src/main/java/com/franchises/develop/repository/BranchRepository.java
+++ src/main/java/com/franchises/develop/repository/BranchRepository.java
@@ -3,8 +3,10 @@ package com.franchises.develop.repository;
 import com.franchises.develop.model.Branch;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface BranchRepository extends ReactiveMongoRepository<Branch, String> {
 
+    Mono<Object> findByName(String name);
 }
