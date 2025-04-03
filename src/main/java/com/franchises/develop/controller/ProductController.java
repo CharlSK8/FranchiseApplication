1e03fffe8312e80d627fbe7efea756dcc7fcd29b
Charlinson Perez <charlinson2013@gmail.com>
2025-04-03 13:19:00 -0500

    docs: Agrega Javadoc para el método updateProductName en ProductController.

--- src/main/java/com/franchises/develop/controller/ProductController.java
+++ src/main/java/com/franchises/develop/controller/ProductController.java
@@ -15,6 +15,17 @@ import reactor.core.publisher.Mono;
 @RequestMapping("api/v1/products")
 @Tag(name = "Products Controller", description = "Endpoints for managing products")
 public class ProductController {
+
+    private IProductService productService;
+
+    /**
+     * Updates the name of a specific product.
+     *
+     * @param productId The unique identifier of the product whose name will be updated.
+     * @param newName   The new name to be assigned to the product.
+     * @return A {@link Mono} emitting a {@link ResponseEntity} containing a {@link ResponseDTO}
+     *         with the updated product information or an error response if the operation fails.
+     */
     @PutMapping("/{productId}/name")
     @Operation(summary = "Update product name", description = "Updates the name of an existing product.")
     public Mono<ResponseEntity<ResponseDTO<Product>>    > updateProductName(@PathVariable String productId, @RequestParam String newName) {
