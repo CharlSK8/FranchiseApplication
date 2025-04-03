d6be389380a1a5f28cf1a7dbd7fc02125879b7cc
Charlinson Perez <charlinson2013@gmail.com>
2025-04-03 13:20:01 -0500

    feat: Crea la interfaz IProductService para definir los métodos de la lógica de negocio relacionados con los productos.

---
+++ src/main/java/com/franchises/develop/service/IProductService.java
@@ -0,0 +1,8 @@
+package com.franchises.develop.service;
+
+import com.franchises.develop.dto.response.ResponseDTO;
+import com.franchises.develop.model.Product;
+import reactor.core.publisher.Mono;
+
+public interface IProductService {
+}
