5855a3b12a14f6fa3a90e58d8cbfea763554d322
Charlinson Perez <charlinson2013@gmail.com>
2025-04-03 23:52:59 -0500

    refactor: Elimina constructor innecesario en BranchNotFoundException

--- src/main/java/com/franchises/develop/exception/handler/BranchNotFoundException.java
+++ src/main/java/com/franchises/develop/exception/handler/BranchNotFoundException.java
@@ -5,8 +5,4 @@ public class BranchNotFoundException extends RuntimeException {
     public BranchNotFoundException(String id) {
         super("No branch found with id " + id +". Please verify the ID and try again.");
     }
-
-    public BranchNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
